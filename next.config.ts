2fa32a5cbf298cd6bb598a57f65b50cacd161c5f
Danny Tan <129268549+DannyTan8x@users.noreply.github.com>
2025-02-24 16:35:55 +0800

    customize for gh-page

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: process.env.PAGES_BASE_PATH,
   assetPrefix: process.env.PAGES_BASE_PATH,
+  images: {
+    unoptimized: true, // Required for GitHub Pages (because it doesn’t support Next.js Image Optimization)
+  },
 };
 
 export default nextConfig;
